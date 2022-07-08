07249935e1ba174ce98aa8cbd50af7c5d180ead4
Sergey <larkov@softved.ru>
2022-07-08 03:49:45 +0300

    third task

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -1,7 +1,19 @@
 
 fun main() {
-    // Задача №2 - Люди/Человеки
-    val likes = 109
-    val txt = if ((likes % 10 == 1)  && (likes != 11) && (likes % 100 != 11)) "человеку" else "людям"
-    println("Понравилось $likes " + txt)
+    // Задача №3 - Меломан
+    val regularСustomer = true
+    val buySum = 25000
+    var result  = buySum.toDouble()
+
+    if ((buySum >= 1001) && (buySum <= 10000)) {
+        result -= 100;
+    } else if (buySum >= 10001) {
+        result = result * 0.95
+    }
+
+    if (regularСustomer) {
+        result = result * 0.99
+    }
+
+    println("Итого сумма покупки: $result руб.");
 }
